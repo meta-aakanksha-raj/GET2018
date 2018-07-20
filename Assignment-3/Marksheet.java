d908bbfffb33ba15aac3fcd77b614a8128e12a71
meta-aakanksha-raj <41181593+meta-aakanksha-raj@users.noreply.github.com>
2018-07-20 02:00:10 -0700

    Update Marksheet.java

--- Assignment-3/Marksheet.java
+++ Assignment-3/Marksheet.java
@@ -2,28 +2,36 @@ import java.util.Scanner;
 class Functions {
 	private double[] marks;
 	private int size;
-	
+	/**
+	 * This constructor for taking number of students
+	 * @param size > 0
+	 */
 	public Functions(int size) {
 		this.size=size;
 	}
-
-	/* This function will work for average marks.
-	If user has entered wrong values then it  will throw ArithmeticExceptiontic*/	
+	/**
+	 * This function gives average of marks 
+	 * @param marks should be between 0 and 100 both
+	 * @return average of marks
+	 * @throws ArithmeticException, if user has entered wrong values
+	 */
 	public double average(double[] marks ) throws ArithmeticException {
 		this.marks=marks;
-		double sum=0;
-		double average_number;
+		double sum=0.0;
 		for(int i=0;i<size;i++) {
 			sum=sum+marks[i];
 		}
 		return sum/size;
 	}
-
-	/* This function will work for finding maximum marks.
-	If user has entered wrong values then it  will throw ArithmeticExceptiontic*/	
+	/**
+	 * This function gives maximum marks
+	 * @param marks should be between 0 and 100 both
+	 * @return maximum of marks
+	 * @throws ArithmeticException, if user has entered wrong values
+	 */
 	public double maximum(double[] marks ) throws ArithmeticException {
 		this.marks=marks;
-		double max=0;
+		double max=0.0;
 		for(int i=0;i<size;i++) {
 			if(marks[i]>max) {
 				max=marks[i];
@@ -31,12 +39,15 @@ class Functions {
 		}
 		return max;
 	}
-	
-	/* This function will work for finding miniimum marks.
-	If user has entered wrong values then it  will throw ArithmeticExceptiontic*/	
+	/**
+	 * This function gives minimum marks
+	 * @param marks should be between 0 and 100 both
+	 * @return minimum of marks
+	 * @throws ArithmeticException, if user has entered wrong values
+	 */
 	public double minimum(double[] marks ) throws ArithmeticException {
 		this.marks=marks;
-		double min=100;
+		double min=100.0;
 		for(int i=0;i<size;i++) {
 			if(marks[i]<min) {
 				min=marks[i];
@@ -44,15 +55,18 @@ class Functions {
 		}
 		return min;
 	}
-
-	/* This function will work for finding percentage of students passed.
-	If user has entered wrong values then it  will throw ArithmeticExceptiontic*/		
+	/**
+	 * This function gives percentage of students who have scored marks>=40
+	 * @param marks should be between 0 and 100 both
+	 * @return percentage of students passed
+	 * @throws ArithmeticException, if user has entered wrong values
+	 */
 	public double percentage_passed(double[] marks ) throws ArithmeticException {
 		this.marks=marks;
 		double count=0.0;
 		double percentage;
 		for(int i=0;i<size;i++) {
-			if(marks[i]>=40) { // Passing mark is 40
+			if(marks[i]>=40) { 
 				count=count+1;
 			}		
 		}
@@ -65,21 +79,19 @@ public class Marksheet {
 	public static void main(String args[]) {
 		Scanner input = new Scanner(System.in);
 		int size;
-		System.out.println("Enter size of students"); // Size should be greater than 0(size>0)
+		System.out.println("Enter size of students"); 
 		size=input.nextInt();
 		Functions object=new Functions(size);
 		
-		double[] marks=new double[size];//Here array of entered size is created for marks storage
+		double[] marks=new double[size];
 		
-		/*Entered marks of each student should be between 0 and 100
-		Otherwise it may give wrong output*/
 		for(int i=0;i<size;i++) {
 			System.out.println("Enter marks for student"+(i+1));
 			marks[i]=input.nextDouble();
 		}
-		
-		/*Choose the operation you want to perform
-		If you have entered wrong input default will run*/
+		/**
+		 * Choose the operation you want to perform, If you have entered wrong input default will run
+		 */
 		while(true) {
 			System.out.println("Choose from following operations: \n 1. Average Marks \n 2. Maximum Marks \n 3. Minimum Marks \n 4. Percentage of Student Passed \n 5. Exit ");
 			int choice;
@@ -88,28 +100,28 @@ public class Marksheet {
 				case 1: {
 					double output;
 					output=object.average(marks);
-					System.out.println("Result of Average: "+output);
+					System.out.printf("Result of Average: %.2f \n", output);
 					break;
 				}
 				case 2: {
 					double output;
 					output=object.maximum(marks);
-					System.out.println("Maximum Marks : "+output);
+					System.out.printf("Maximum Marks : %.2f \n", output);
 					break;
 				}
 				case 3: {
 					double output;
 					output=object.minimum(marks);
-					System.out.println("Minimum Marks : "+output);
+					System.out.printf("Minimum Marks : %.2f \n", output);
 					break;
 				}
 				case 4: {
 					double output;
 					output=object.percentage_passed(marks);
-					System.out.println(" Percentage passed : "+output);
+					System.out.printf(" Percentage passed : %.2f \n", output);
 					break;
 				}
-				case 5: { //This will take you out of the loop
+				case 5: { 
 					System.exit(0);
 				}
 				default: System.out.println("Wrong input!! Try again!!");
