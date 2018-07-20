8ef9ba9fbf697896860d2b84cabff46af5b7cab5
meta-aakanksha-raj <41181593+meta-aakanksha-raj@users.noreply.github.com>
2018-07-20 01:59:30 -0700

    Update Area.java

--- Assignment-3/Area.java
+++ Assignment-3/Area.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
 class Shapes {
 	private double result;
-	/* This function will work for triangle.
-	If user has entered wrong values then it  will throw ArithmeticExceptiontic*/
-	public double traingle(double base,double height) throws ArithmeticException {
-		double base_traingle=base;
+	/**
+	 * This function gives area of triangle
+	 * @param width > 0
+	 * @param height > 0
+	 * @return area of triangle in double
+	 * @throws ArithmeticException, if user has entered wrong values
+	 */
+	public double traingle(double width,double height) throws ArithmeticException {
+		double base_traingle=width;
 		double height_traingle=height;
 		double area;
-		area=0.5*base_traingle*height_traingle;
+		area=0.5*base_traingle*height_traingle;   
 		return area;
 	}
-
-	/* This function will work for rectangle.
-	If user has entered wrong values then it  will throw ArithmeticException*/	
+	/**
+	 * This function gives area of rectangle
+	 * @param width > 0
+	 * @param height >0
+	 * @return area of rectangle in double
+	 * @throws ArithmeticException, if user has entered wrong values
+	 */
 	public double rectangle(double width,double height) throws ArithmeticException {
 		double width_rectangle=width;
 		double height_rectangle=height;
@@ -20,18 +29,24 @@ class Shapes {
 		area=width_rectangle*height_rectangle;
 		return area;
 	}
-	
-	/* This function will work for square.
-	If user has entered wrong value then it  will throw ArithmeticException*/
+	/**
+	 * This function gives area of square
+	 * @param width > 0
+	 * @return area of square in double
+	 * @throws ArithmeticException, if user has entered wrong values
+	 */
 	public double square(double width) throws ArithmeticException {
 		double width_square=width;
 		double area;
 		area=width_square*width_square;
 		return area;
 	}
-
-	/* This function will work for circle.
-	If user has entered wrong values then it  will throw ArithmeticException*/	
+	/**
+	 * This function gives area of circle
+	 * @param radius > 0
+	 * @return area of circle in double
+	 * @throws ArithmeticException
+	 */
 	public double circle(double radius) throws ArithmeticException {
 		double radius_circle=radius;
 		double area;
@@ -44,22 +59,24 @@ public class Area {
 	public static void main(String args[]) {
 		Scanner input = new Scanner(System.in);
 		Shapes object=new Shapes();
-		/* Different shapes are provided here.
-		Choose what shape you want */
+		/**
+		 * Different shapes are provided here.Choose what shape you want.
+		 */
 		while(true) {
 			System.out.println("\n Choose from following Shapes: \n 1. Triangle \n 2. Rectangle \n 3. Square \n 4. Circle \n 5. Exit ");
 			int choice;
 			choice=input.nextInt();
-			/* Enter values for width, height and radius greater than zero (>0), 
-			otherwise it may give incorrect output */
+			/**
+			 * Enter values for width, height and radius greater than zero (>0),otherwise it may give incorrect output
+			 */
 			switch(choice) {
 				case 1: {
-					double base,height,output;
+					double width,height,output;
 					System.out.println("Enter base of traingle");
-					base=input.nextDouble();
+					width=input.nextDouble();
 					System.out.println("Enter height of triangle");
 					height=input.nextDouble();					
-					output=object.traingle(base,height);
+					output=object.traingle(width,height);
 					System.out.println("Area of Traingle: "+output);
 					break;
 				}
@@ -92,7 +109,7 @@ public class Area {
 				case 5: {
 					System.exit(0);
 				}
-				default: System.out.println("Wrong input!! Try again!!");//If user has entered wrong input then default case will run 
+				default: System.out.println("Wrong input!! Try again!!");
 			}
 		}
 	}
