16e4ab08f14967f995bb3b312185abe6aa1aa2cb
Aakanksha <aakanksha.raj@metacube.com>
2018-07-23 17:14:25 -0700

    changes

--- Assignment4/Original/test/ArrOperation.java
+++ Assignment4/Original/test/ArrOperation.java
@@ -12,7 +12,7 @@ public class ArrOperation {
 			throw new AssertionError("Array is empty");
 		}
 		int count=0;
-		for(int i=0;i<arrayLength-1;i++) {
+		for(int i=0;i<arrayLength-1;i++) {    
 			if(array[i]==array[i+1]) {
 				count++;	
 				for(int j=i+1;j<arrayLength-1;j++) {
