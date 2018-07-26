bcb964ea263e4893aa5b30966e589464998c3a55
meta-aakanksha-raj <41181593+meta-aakanksha-raj@users.noreply.github.com>
2018-07-27 03:01:44 +0530

    Update MainPoly.java

--- Assignment-6/polynomial/MainPoly.java
+++ Assignment-6/polynomial/MainPoly.java
@@ -3,24 +3,70 @@ package polynomial;
 import java.util.Scanner;
 
 public class MainPoly {
-	public static void main(String args[]){
-		Scanner input=new Scanner(System.in);
-		
-		Poly object=new Poly(new int[][]{{2,1,0},{4,3,1}});
-		//Poly object=new Poly(new int[][]{{4,2,1,0},{3,4,2,6}});
-		//Poly object=new Poly(0,new int[][]{{},{}});
-		//Poly object=new Poly(4,new int[][]{{4,2,1,0},{3,4,0,6}});
-		
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the size of polynomial array");
+		int size = input.nextInt();
+		int[][] poly = new int[2][size];
+		System.out.println("Enter all power first ");
+		for (int i = 0; i < size; i++) {
+			poly[0][i] = input.nextInt();
+		}
+		System.out.println("Enter all coefficients ");
+		for (int i = 0; i < size; i++) {
+			poly[1][i] = input.nextInt();
+		}
+
+		// Creating object
+		Poly object = new Poly(poly);
+
+		// Enter variable value
 		System.out.println("Enter the value of variable");
-		double value=input.nextDouble();
-		double result=object.evaluate(value);
-		System.out.println("Result= " +result);
-		
-		int output=object.degree();
+		double value = input.nextDouble();
+		double result = object.evaluate(value);
+		System.out.println("Result= " + result);
+
+		// To get degree of polynomial
+		int output = object.degree();
 		System.out.println("Degree= " + output);
-		/*
-		Poly p1=new Poly(4,new int[][]{{4,2,1,0},{3,4,2,6}});
-		Poly p2=new Poly(4,new int[][]{{4,2,1,0},{3,4,2,6}});
-		int[][] adding=p1.addPoly(p1,p2);*/
+
+		// creating first polynomial for addition and multiplication
+		System.out
+				.println("Enter the size of polynomial first polynomial array ");
+		int sizeFirst = input.nextInt();
+		int[][] polyOne = new int[2][sizeFirst];
+		System.out.println("Enter all power first ");
+		for (int i = 0; i < sizeFirst; i++) {
+			polyOne[0][i] = input.nextInt();
+		}
+		System.out.println("Enter all coefficients ");
+		for (int i = 0; i < sizeFirst; i++) {
+			polyOne[1][i] = input.nextInt();
+		}
+
+		// creating second polynomial for addition and multiplication
+		System.out
+				.println("Enter the size of polynomial second polynomial array ");
+		int sizeSecond = input.nextInt();
+		int[][] polyTwo = new int[2][sizeSecond];
+		System.out.println("Enter all power first ");
+		for (int i = 0; i < sizeSecond; i++) {
+			polyTwo[0][i] = input.nextInt();
+		}
+		System.out.println("Enter all coefficients ");
+		for (int i = 0; i < sizeSecond; i++) {
+			polyTwo[1][i] = input.nextInt();
+		}
+
+		// Creating object
+		Poly p1 = new Poly(polyOne);
+		Poly p2 = new Poly(polyTwo);
+		Poly p3 = new Poly();
+
+		// For addition and multiplication of polynomial
+		System.out.println("Addition array of polynomial ");
+		int[][] addArray = p3.addPoly(p1, p2);
+		System.out.println("Multiplication array of polynomial ");
+		int[][] multArray = p3.multPoly(p1, p2);
 	}
 }
