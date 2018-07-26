a7147b63e4589c0030fca88acc32bd85c12ad39e
meta-aakanksha-raj <41181593+meta-aakanksha-raj@users.noreply.github.com>
2018-07-26 00:01:40 -0700

    Update NQueenRecursion.java

--- Assignment-5/recursion/NQueenRecursion.java
+++ Assignment-5/recursion/NQueenRecursion.java
@@ -4,45 +4,45 @@ public class NQueenRecursion {
 
 	/**
 	 * Here chessboard is created according to the number of queens to be placed
-	 * @param N for taking value for number of queens to be placed
+	 * @param dimensionOfMatrix is the dimension of board
 	 * @return the array of chessboard on which queen is placed if correct value is taken else is empty
 	 */
-	public int[][] numberOfQueens(int N) {
-		int row = 0;
-		int[][] chessBoard = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				chessBoard[i][j] = 0;
+	public int[][] numberOfQueens(int dimensionOfMatrix) {
+		int startRow = 0;
+		int[][] board = new int[dimensionOfMatrix][dimensionOfMatrix];
+		for (int i = 0; i < dimensionOfMatrix; i++) {
+			for (int j = 0; j < dimensionOfMatrix; j++) {
+				board[i][j] = 0;
 			}
 		}
-		boolean result = nQueen(chessBoard, row, N);
-		return chessBoard;
+		boolean result = nQueen(board, startRow, dimensionOfMatrix);
+		return board;
 	}
 
 	/**
 	 * This method is to get all the positions where queen can be placed
-	 * @param chessBoard is 2-D array in which we have to place queen
+	 * @param board is 2-D array in which we have to place queen
 	 * @param row is for value of each row to check
-	 * @param N is number of queens
+	 * @param dimensionOfMatrix is the dimension of board
 	 * @return
 	 */
-	private boolean nQueen(int[][] chessBoard, int row, int N) {
+	private boolean nQueen(int[][] board, int row, int dimensionOfMatrix) {
 		boolean placed = false;
 
-		for (int column = 0; column < N; column++) {
-			placed = canBePlaced(chessBoard, row, column, N);
+		for (int column = 0; column < dimensionOfMatrix; column++) {
+			placed = canBePlaced(board, row, column, dimensionOfMatrix);
 
 			if (placed) {
-				chessBoard[row][column] = 1;
+				board[row][column] = 1;
 				row++;
 
-				if (row == N) {
+				if (row == dimensionOfMatrix) {
 					placed = true;
 					break;
 				}
-				if (!nQueen(chessBoard, row, N)) {
+				if (!nQueen(board, row, dimensionOfMatrix)) {
 					row--;
-					chessBoard[row][column] = 0;
+					board[row][column] = 0;
 					placed = false;
 				} else {
 					break;
@@ -54,34 +54,35 @@ public class NQueenRecursion {
 
 	/**
 	 * To test whether at particular position queen can be placed or not
-	 * @param chessBoard is 2-D array in which we have to place queen
+	 * @param board is 2-D array in which we have to place queen
 	 * @param row is for value of each row to check
 	 * @param column is for value of each column to check
-	 * @param N is number of queens
+	 * @param dimensionOfMatrix is the dimension of board
 	 * @return result whether queen can be placed or not 
 	 */
-	private boolean canBePlaced(int[][] chessBoard, int row, int column, int N) {
+	private boolean canBePlaced(int[][] board, int row, int column, int dimensionOfMatrix) {
 		int i, j;
+		boolean result=true;
 		
 		//checks if any queen exists in the same column
 		for (i = 0; i < row; i++) {
-			if (chessBoard[i][column] == 1) {
-				return false;
+			if (board[i][column] == 1) {
+				result=false;
 			}
 		}
 		
 		//checks if any queen exists in the upper left diagonal
 		for (i = row, j = column; i >= 0 && j >= 0; i--, j--) {
-			if (chessBoard[i][j] == 1) {
-				return false;
+			if (board[i][j] == 1) {
+				result=false;
 			}
 		}
 		//checks if any queen exists in the upper right diagonal
-		for (i = row, j = column; i >= 0 && j < N; i--, j++) {
-			if (chessBoard[i][j] == 1) {
-				return false;
+		for (i = row, j = column; i >= 0 && j < dimensionOfMatrix; i--, j++) {
+			if (board[i][j] == 1) {
+				result=false;
 			}
 		}
-		return true;
+		return result;
 	}
 }
