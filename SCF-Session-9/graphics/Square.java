6536abde8b06f482afdcc1ac72823b7ac0c01d6c
meta-aakanksha-raj <41181593+meta-aakanksha-raj@users.noreply.github.com>
2018-08-03 08:21:16 +0530

    Update Square.java

--- SCF-Session-9/graphics/Square.java
+++ SCF-Session-9/graphics/Square.java
@@ -1,34 +1,78 @@
 package graphics;
 
-public class Square implements Shape{
-	private int width;
-	private Point point;
-	
-	public Square(Point point, int values) {
-		this.width = values;
-		this.point = point;
+import java.util.Date;
+
+/**
+ * this class implements the Shape class to make objects of ShapeType square
+ *
+ */
+public class Square implements Shape {
+
+	public Date timestamp = null; // time at which object is added to screen
+	private float width;
+	private Point origin;
+
+	// Constructor of class
+	public Square(Point origin, float width) {
+		this.width = width;
+		this.origin = origin;
 	}
-	
+
 	@Override
-	public double getArea() {
-	
+	public float getArea() {
+
 		return width * width;
 	}
 
 	@Override
-	public double getPerimeter() {
-		
+	public float getPerimeter() {
+
 		return 4 * width;
 	}
 
 	@Override
 	public Point getOrigin() {
-		return new Point(point.getxCoordinate(), point.getyCoordinate());
+		return new Point(origin.getXCoordinate(), origin.getYCoordinate());
 	}
-	
+
+	@Override
+	public boolean isPointEnclosed(Point isEnclosedPoint) {
+
+		boolean result = false;
+		float xOrigin = origin.getXCoordinate();
+		float yOrigin = origin.getYCoordinate();
+		float xPoint = isEnclosedPoint.getXCoordinate();
+		float yPoint = isEnclosedPoint.getYCoordinate();
+		if (xPoint > xOrigin && xPoint < xOrigin + width && yPoint > yOrigin
+				&& yPoint < yOrigin + width) {
+			result = true;
+		}
+		return result;
+	}
+
 	@Override
 	public ShapeEnum getShapeType() {
-		
+
 		return ShapeEnum.SQUARE;
+	}
+
+	@Override
+	public float getOriginDistance() { // distance between origin of the shape
+										// and origin of the screen
+
+		return (float) Math.sqrt((Math.pow(origin.getXCoordinate(), 2) + Math
+				.pow(origin.getYCoordinate(), 2)));
+	}
+
+	@Override
+	public Date getTimestamp() {
+
+		return timestamp;
+	}
+
+	@Override
+	public void setTimestamp(Date timestamp) {
+
+		this.timestamp = timestamp;
 	}
 }
