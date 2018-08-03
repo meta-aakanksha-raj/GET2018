fa3b87803906073410fae6cf70492a1bccaf2c60
meta-aakanksha-raj <41181593+meta-aakanksha-raj@users.noreply.github.com>
2018-08-03 14:39:46 +0530

    Update Reptile.java

--- SCF-Session-8/system/Reptile.java
+++ SCF-Session-8/system/Reptile.java
@@ -1,9 +1,23 @@
 package system;
 
+/**
+ * Reptile is the subclass of Animal and it adds the Reptile type animal into Zoo
+ *
+ */
 public class Reptile extends Animal{
 	
 	private final boolean isColdBlood;
-	
+
+	/**
+	 * This initializes the Reptile properties and calls super to add it into animal
+	 * @param name
+	 * @param age
+	 * @param weight
+	 * @param sound
+	 * @param noOfLegs
+	 * @param isColdBlood
+	 * @param type
+	 */
 	public Reptile(String name,int age, double weight,String sound, int noOfLegs,boolean isColdBlood,AnimalType type) {
 		
 		super(name,age,weight,sound,noOfLegs,AnimalCategory.REPTILE,type);
@@ -13,6 +27,4 @@ public class Reptile extends Animal{
 	public boolean isColdBlood() {
 		return isColdBlood;
 	}
-	
-
 }
