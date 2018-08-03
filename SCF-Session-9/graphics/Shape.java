d7a493473f98038a499c5be7ba3c8bde964abce5
meta-aakanksha-raj <41181593+meta-aakanksha-raj@users.noreply.github.com>
2018-08-03 08:19:51 +0530

    Update Shape.java

--- SCF-Session-9/graphics/Shape.java
+++ SCF-Session-9/graphics/Shape.java
@@ -1,15 +1,58 @@
 package graphics;
 
+import java.util.Date;
+
+/**
+ * interface for creating shape objects
+ *
+ */
 public interface Shape {
 
-	public double getArea();
+	/**
+	 * calculates the area of the shape
+	 * 
+	 * @return area
+	 */
+	public float getArea();
 
-	public double getPerimeter();
+	/**
+	 * calculates the perimeter of the shape
+	 * 
+	 * @return perimeter
+	 */
+	public float getPerimeter();
 
+	/**
+	 * @return origin of the shape
+	 */
 	public Point getOrigin();
 
-	public boolean isPointEnclosed();
+	/**
+	 * checks if the given point lies inside the shape or not
+	 */
+	public boolean isPointEnclosed(Point isEnclosedPoint);
 
+	/**
+	 * @return ShapeType of the shape
+	 */
 	public ShapeEnum getShapeType();
+
+	/**
+	 * @return originDistance, i.e the distance between origin of the shape and
+	 *         origin of the screen
+	 */
+	public float getOriginDistance();
+
+	/**
+	 * @return timestamp of the shape
+	 */
+	public Date getTimestamp();
+
+	/**
+	 * sets the timestamp at which the object is added to the screen
+	 * 
+	 * @param timestamp
+	 */
+	public void setTimestamp(Date timestamp);
 
 }
