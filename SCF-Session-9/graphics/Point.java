4c23ea4ba823d870b4260d94ddf7c828852f8b14
meta-aakanksha-raj <41181593+meta-aakanksha-raj@users.noreply.github.com>
2018-08-03 08:16:22 +0530

    Update Point.java

--- SCF-Session-9/graphics/Point.java
+++ SCF-Session-9/graphics/Point.java
@@ -1,26 +1,30 @@
 package graphics;
 
+/**
+ * This class represents coordinates
+ *
+ */
 public class Point {
-	
-	private int xCoordinate;
-	private int yCoordinate;
-	
-	 public Point(int xCoordinate,int yCoordinate) {
-		 this.xCoordinate=xCoordinate;
-		 this.yCoordinate=yCoordinate;
-	 }
+
+	private float xCoordinate, yCoordinate; // to store x and y coordinates
+
+	// Constructor of class
+	public Point(float xCoordinate, float yCoordinate) {
+		this.xCoordinate = xCoordinate;
+		this.yCoordinate = yCoordinate;
+	}
 
 	/**
 	 * @return the xCoordinate
 	 */
-	public int getxCoordinate() {
+	public float getXCoordinate() {
 		return xCoordinate;
 	}
 
 	/**
 	 * @return the yCoordinate
 	 */
-	public int getyCoordinate() {
+	public float getYCoordinate() {
 		return yCoordinate;
 	}
 }
