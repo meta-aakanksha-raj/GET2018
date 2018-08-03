847ed9745de7a2ba6d4435f316d14a4e9e0b5b95
meta-aakanksha-raj <41181593+meta-aakanksha-raj@users.noreply.github.com>
2018-08-03 08:17:10 +0530

    Update Polygon.java

--- SCF-Session-9/graphics/Polygon.java
+++ SCF-Session-9/graphics/Polygon.java
@@ -1,5 +1,75 @@
 package graphics;
 
-public class Polygon {
+import java.util.Date;
 
+/**
+ * this class implements the Shape class to make objects of Shape Type Regular
+ * Polygon
+ *
+ */
+public class Polygon implements Shape {
+
+	public Date timestamp = null; // time at which object is added to screen
+	private int noOfSides;
+	private float lengthOfSide;
+	private Point origin;
+
+	// Constructor of class
+	public Polygon(Point origin, float noOfSides, float lengthOfSide) {
+		this.noOfSides = (int) noOfSides;
+		this.lengthOfSide = lengthOfSide;
+		this.origin = origin;
+	}
+
+	@Override
+	public float getArea() {
+
+		return (float) ((getPerimeter() * getApothem()) / 2.0);
+	}
+
+	@Override
+	public float getPerimeter() {
+
+		return lengthOfSide * noOfSides;
+	}
+
+	@Override
+	public Point getOrigin() {
+
+		return new Point(origin.getXCoordinate(), origin.getYCoordinate());
+	}
+
+	@Override
+	public boolean isPointEnclosed(Point isEnclosedPoint) {
+
+		return false;
+	}
+
+	@Override
+	public ShapeEnum getShapeType() {
+
+		return ShapeEnum.REGULARPOLYGON;
+	}
+
+	@Override
+	public float getOriginDistance() { // distance between origin of the shape
+										// and origin of the screen
+
+		return (float) Math.sqrt(Math.pow(origin.getXCoordinate(), 2)
+				+ Math.pow(origin.getYCoordinate(), 2));
+	}
+
+	@Override
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	@Override
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	private float getApothem() {
+		return (float) (lengthOfSide / (2 * Math.tan(180 / noOfSides)));
+	}
 }
