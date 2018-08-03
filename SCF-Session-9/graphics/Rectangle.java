a8fba7bb93523a84c719995a3e6bd26141d536e8
meta-aakanksha-raj <41181593+meta-aakanksha-raj@users.noreply.github.com>
2018-08-03 08:18:12 +0530

    Update Rectangle.java

--- SCF-Session-9/graphics/Rectangle.java
+++ SCF-Session-9/graphics/Rectangle.java
@@ -1,37 +1,79 @@
 package graphics;
 
-public class Rectangle implements Shape{
+import java.util.Date;
 
-	private int breadth;
-	private int length;
-	private Point point;
-	
-	public Rectangle(Point point, int length, int breadth) {
-		this.point = point;
+/**
+ * this class implements the Shape class to make objects of Shape Type Rectangle
+ *
+ */
+public class Rectangle implements Shape {
+
+	public Date timestamp = null; // time at which object is added to screen
+	private float breadth;
+	private float length;
+	private Point origin;
+
+	// Constructor of class
+	public Rectangle(Point origin, float length, float breadth) {
+		this.origin = origin;
 		this.length = length;
 		this.breadth = breadth;
 	}
-	
+
 	@Override
-	public double getArea() {
-		
+	public float getArea() {
+
 		return length * breadth;
 	}
 
 	@Override
-	public double getPerimeter() {
-		
+	public float getPerimeter() {
+
 		return 2 * (length + breadth);
 	}
 
 	@Override
 	public Point getOrigin() {
-		
-		return new Point(point.getxCoordinate(), point.getyCoordinate());
+
+		return new Point(origin.getXCoordinate(), origin.getYCoordinate());
 	}
+
+	@Override
+	public boolean isPointEnclosed(Point isEnclosedPoint) {
+
+		boolean result = false;
+		float xOrigin = origin.getXCoordinate();
+		float yOrigin = origin.getYCoordinate();
+		float xPoint = isEnclosedPoint.getXCoordinate();
+		float yPoint = isEnclosedPoint.getYCoordinate();
+		if (xPoint > xOrigin && xPoint < xOrigin + length && yPoint > yOrigin
+				&& yPoint < yOrigin + breadth) {
+			result = true;
+		}
+		return result;
+	}
+
 	@Override
 	public ShapeEnum getShapeType() {
-		
+
 		return ShapeEnum.RECTANGLE;
-	}	
+	}
+
+	@Override
+	public float getOriginDistance() // distance between origin of the shape and
+										// origin of the screen
+	{
+		return (float) Math.sqrt((Math.pow(origin.getXCoordinate(), 2) + Math
+				.pow(origin.getYCoordinate(), 2)));
+	}
+
+	@Override
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	@Override
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
 }
