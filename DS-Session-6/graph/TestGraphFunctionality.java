f7789d4e90126afe528689877861698bd9c30ed0
Aakanksha Raj <41181593+meta-aakanksha-raj@users.noreply.github.com>
2018-08-16 17:56:09 +0530

    Update TestGraphFunctionality.java

--- DS-Session-6/graph/TestGraphFunctionality.java
+++ DS-Session-6/graph/TestGraphFunctionality.java
@@ -38,23 +38,6 @@ public class TestGraphFunctionality {
 	}
 
 	@Test
-	public void testReachableMethodOfGraphWhenSourceIsProvided() {
-
-		GraphFunctionality graph = new GraphFunctionality(6);
-		graph.addEdge(0, 1, 2);
-		graph.addEdge(1, 2, 6);
-		graph.addEdge(2, 3, 5);
-		graph.addEdge(3, 0, 4);
-		graph.addEdge(4, 5, 4);
-
-		List<Integer> reachable = graph.reachable(5);
-
-		for (Integer i : reachable) {
-			System.out.println(i);
-		}
-	}
-
-	@Test
 	public void testShortestPathMethodBetweenTwoVertices() {
 
 		GraphFunctionality graph = new GraphFunctionality(4);
@@ -65,21 +48,5 @@ public class TestGraphFunctionality {
 
 		assertEquals(8, graph.shortestPath(0, 2));
 
-	}
-
-	@Test
-	public void testMinimumSpanningTreeMethod() {
-
-		GraphFunctionality graph = new GraphFunctionality(4);
-		graph.addEdge(0, 1, 2);
-		graph.addEdge(1, 2, 6);
-		graph.addEdge(2, 3, 5);
-		graph.addEdge(3, 0, 4);
-		graph.addEdge(0, 2, 2);
-
-		List<Edge> g = graph.minimumSpanningTree();
-		for (Edge e : g) {
-			System.out.println(e.getSource() + " " + e.getDestination() + " " + e.getWeight());
-		}
 	}
 }
