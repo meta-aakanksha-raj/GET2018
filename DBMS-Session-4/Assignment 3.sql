e66d2162c8ab3ee2fb4fbdb59dea147e70024f2c
Aakanksha <aakanksha.raj@metacube.com>
2018-08-20 23:25:18 +0530

    DBMS-Session-4

---
+++ DBMS-Session-4/Assignment 3.sql
@@ -0,0 +1,44 @@
+USE storefront;
+
+SHOW TABLES;
+
+#CATEGORIES
+#Creating ParentCategory in CATEGORIES As Index.
+DESCRIBE CATEGORIES;
+
+CREATE INDEX ParentCategoryIndex
+ON CATEGORIES (ParentCategory);
+
+DESCRIBE CATEGORIES;
+
+
+#PRODUCTS
+#Creating BrandName in PRODUCTS As INDEX
+DESCRIBE PRODUCTS;
+
+CREATE INDEX ProductBrandIndex
+ON PRODUCTS (Brand);
+
+#Creating BrandName in PRODUCTS As INDEX
+CREATE INDEX ProductNameIndex
+ON PRODUCTS (Name);
+
+DESCRIBE PRODUCTS;
+
+
+#ORDERS
+#Creating UserID as INDEX IN ORDERS table
+DESCRIBE ORDERS;
+
+CREATE INDEX UserIDIndex
+ON ORDERS(UserID);
+
+#Creating AddressID as INDEX IN ORDERS table
+CREATE INDEX AddressIDIndex
+ON ORDERS(AddressID);
+
+#Creating PlacedDate as INDEX IN ORDERS table
+CREATE INDEX PlacedDateIndex
+ON ORDERS(PlacedDate);
+
+DESCRIBE ORDERS;
