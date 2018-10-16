e41ee591ef01ca41e533dab0f420de62e2a5099e
Aakanksha <aakanksha.raj@metacube.com>
2018-10-16 16:32:52 +0530

    UI-Session-10

--- UI-Session-10/UserManagement/app/user-list/user-list.component.js
+++ UI-Session-10/UserManagement/app/user-list/user-list.component.js
@@ -4,51 +4,43 @@ angular.module('userList').
 component('userList',{
   templateUrl: 'user-list/user-list.template.html',
   controller: function UserListController($scope,$http){
-
+    $scope.userData;
     var self= this;
 
-    $http.get('http://localhost:3000/users').then(function(response){
-      self.users = response.data;
+    function hello() { 
+      $http.get('http://localhost:3000/users').then(function(response){
+        self.users = response.data;
+      })};
+
+      hello();          
+      $scope.postData=function(){
+       if($scope.user.id == null){
+        console.log("Post data");
+        $http({
+          method: 'POST',
+          url:'http://localhost:3000/users/',
+          data: $scope.user,
+          dataType:'json'
+        });
+      }else{
+        console.log("Put data");
+        $http.put('http://localhost:3000/users/' + $scope.user.id,$scope.user);
+      }
+      console.log("finally");
+      angular.element("#modalAddUser").modal("hide");
+      hello();
+    }
+
+    $scope.editUser = function(id){
+     console.log(id);
+     console.log("Id get");
+     $http({
+      method: 'GET',
+      url: 'http://localhost:3000/users/' + id
+    }).then(function (response){
+      $scope.user = response.data;
     });
-
-            /*$scope.postData=function(){
-                $http({
-                    method:'POST',
-                    url:'http://localhost:3000/users/',
-                    data: $scope.user,
-                    dataType:'json'
-                });
-
-                window.location.reload();
-              };*/
-              $scope.postData=function(){
-               if($scope.user.id == null){
-                console.log("Post data");
-                $http({
-                  method: 'POST',
-                  url:'http://localhost:3000/users/',
-                  data: $scope.user,
-                  dataType:'json'
-                });
-              }else{
-                console.log("Put data");
-                $http.put('http://localhost:3000/users/' + $scope.user.id,$scope.user);
-              }
-              console.log("finally");
-
-              window.location.reload();
-            }
-
-            $scope.editUser = function(id){
-             console.log(id);
-             console.log("Id get");
-             $http({
-              method: 'GET',
-              url: 'http://localhost:3000/users/' + id
-            }).then(function (response){
-              $scope.user = response.data;
-            });
-            console.log("finally edit");
-          }
-        }
-      });
+    console.log("finally edit");
+  }
+}
+});
