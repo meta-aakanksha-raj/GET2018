9c4d09e721323a723289f62c0f2ef16c7364a468
Aakanksha <aakanksha.raj@metacube.com>
2018-10-17 18:06:31 +0530

    UI-Session-10

--- UI-Session-10/UserManagement/app/user-list/user-list.component.js
+++ UI-Session-10/UserManagement/app/user-list/user-list.component.js
@@ -7,40 +7,50 @@ component('userList',{
     $scope.userData;
     var self= this;
 
-    function hello() { 
-      $http.get('http://localhost:3000/users').then(function(response){
-        self.users = response.data;
-      })};
-
-      hello();          
-      $scope.postData=function(){
-       if($scope.user.id == null){
-        console.log("Post data");
-        $http({
-          method: 'POST',
-          url:'http://localhost:3000/users/',
-          data: $scope.user,
-          dataType:'json'
-        });
-      }else{
-        console.log("Put data");
-        $http.put('http://localhost:3000/users/' + $scope.user.id,$scope.user);
-      }
-      console.log("finally");
-      angular.element("#modalAddUser").modal("hide");
-      hello();
-    }
-
-    $scope.editUser = function(id){
-     console.log(id);
-     console.log("Id get");
-     $http({
-      method: 'GET',
-      url: 'http://localhost:3000/users/' + id
-    }).then(function (response){
-      $scope.user = response.data;
+    
+    $http.get('http://localhost:3000/users').then(function(response){
+      self.users = response.data;
     });
-    console.log("finally edit");
+
+
+    $scope.postData=function(){
+     if($scope.user.id == null){
+      console.log("Post data");
+      $http({
+        method: 'POST',
+        url:'http://localhost:3000/users/',
+        data: $scope.user,
+        dataType:'json'
+      }).then((data) => {
+        self.users.push(data.data);
+      });
+    }else{
+      console.log("Put data");
+      $http.put('http://localhost:3000/users/' + $scope.user.id,$scope.user).then((data) => {
+        self.users.push(data.data);
+      });
+    }
+    console.log("finally");
+    angular.element("#modalAddUser").modal("hide");
   }
+
+  $scope.editUser = function(id){
+   console.log(id);
+   console.log("Id get");
+   $http({
+    method: 'GET',
+    url: 'http://localhost:3000/users/' + id
+  }).then(function (response){
+    $scope.user = response.data;
+  });
+  console.log("finally edit");
+}
+$scope.resetUser = function(){
+  $scope.user.id = null;  
+  $scope.user.name = null;
+  $scope.user.location = null;
+  $scope.user.phone = null;
+
+}
 }
 });
